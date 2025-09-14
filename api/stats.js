2e5a8f682e6cfc34f2f36b12e015e7c27e149365
oRix012 <70334905+RAMICHE639@users.noreply.github.com>
2025-09-14 22:39:22 +0100

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,7 +1,7 @@
 
   export default async function handler(req, res) {
     try {
-      const response = await fetch("https://api.goaffpro.com/v1/admin/stats", {
+      const response = await fetch("GET https://api.goaffpro.com/v1/admin/analytics/overview", {
         headers: {
           "e927b41f9f486dc60ecfdabc20c6af3a1244e6d3e7467fa25f497da2998ebd24": process.env.GOAFFPRO_ACCESS_TOKEN,
           "e927b41f9f486dc60ecfdabc20c6af3a1244e6d3e7467fa25f497da2998ebd24": process.env.GOAFFPRO_PUBLIC_TOKEN,
